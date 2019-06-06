b5795c7d063a83d174ddb6e19ac5d43b04ea343e
itzfeltrin <pedrofeltrin45@yahoo.com>
2019-06-06 01:29:13 -0300

    first class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@
  */
 package application;
 
+import boardgame.Position;
+
 /**
  *
  * @author itzfeltrin
@@ -15,7 +17,8 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
     
 }
---
+++ src/boardgame/Position.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boardgame;
+
+/**
+ *
+ * @author itzfeltrin
+ */
+public class Position {
+    private Integer row;
+    private Integer column;
+    
+    public Position(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+    
+    @Override
+    public String toString(){
+        return this.row + ", " + this.column;
+    }
+}
